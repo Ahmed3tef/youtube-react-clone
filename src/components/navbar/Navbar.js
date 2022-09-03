b0e0cf6a08460a39b678eae9c5aad02b1e0eea9a
Ahmed Osama <67006758+aosama314@users.noreply.github.com>
2022-09-03 16:21:41 +0200

    Fix navbar logo and styles (#1)

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,20 +1,24 @@
-import { MdMenu } from 'react-icons/md';
-import Icon from '../icon/Icon';
+import { MdMenu } from "react-icons/md";
+import Icon from "../icon/Icon";
 
-import './_navbar.scss';
+import "./_navbar.scss";
+import LogoImg from "../../img/logo.png";
 
-const Navbar = () => (
-  <div className='navbar'>
-    <div className='logo__container'>
-      <Icon className='navbar__menu icon'>
-        <MdMenu />
-      </Icon>
-      <div className='logo'>
-        <img src='../../img/logo.png' alt='logo' className='logo__img' />
-        <span className='logo__text'>YouTube</span>
+const Navbar = () => {
+  console.log(LogoImg);
+  return (
+    <div className="navbar">
+      <div className="logo__container">
+        <Icon className="navbar__menu icon">
+          <MdMenu />
+        </Icon>
+        <div className="logo">
+          <img src={LogoImg} alt="logo" className="logo__img" />
+          <span className="logo__text">YouTube</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
